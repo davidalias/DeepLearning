ab3257d801d3bbf9a8af05e28eadb4b8fd95fedc
Davis Boban <davisboban2k17@gmail.com>
2026-06-22 18:59:42 +0200

    fastapi creation for potato_disease

---
+++ CNN/project/potato_disease/api/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+from io import BytesIO
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model("../models/1")
+CLASS_NAMES = ['Early Blight', "Late Blight", "Healthy"]
+
+@app.get("/ping") #ping is the endpoint
+async def ping():
+    return "Hello I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = read_file_as_image(await file.read()) 
+
+    image_batch = np.expand_dims(image, 0) #add one more dim at column level
+    prediction = MODEL.predict(image_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port = 8000, host="localhost")
